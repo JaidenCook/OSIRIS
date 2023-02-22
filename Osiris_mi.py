c70da8ee6b64da9797d5ba710dfbcf637683213e
JaidenCook <jaiden.cook@student.curtin.edu.au>
2023-02-22 15:08:12 +0800

    Added mutual information module file.

---
+++ Osiris_mi.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+__author__ = "Jaiden Cook"
+__credits__ = ["Jaiden Cook"]
+__version__ = "1.0.0"
+__maintainer__ = "Jaiden Cook"
+__email__ = "Jaiden.Cook@student.curtin.edu"
+
+# Generic stuff:
+#%matplotlib notebook
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+from tqdm import tqdm
+
+# Array stuff:
+import numpy as np
+warnings.simplefilter('ignore', np.RankWarning)
+
+# Plotting stuff:
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.style.use('seaborn-white')
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams.update({'font.size': 12})
+
+plt.rc('xtick', color='k', labelsize='medium', direction='out')
+plt.rc('xtick.major', size=6, pad=4)
+plt.rc('xtick.minor', size=4, pad=4)
+
+plt.rc('ytick', color='k', labelsize='medium', direction='out')
+plt.rc('ytick.major', size=6, pad=4)
+plt.rc('ytick.minor', size=4, pad=4)
+
+# Parser options:
+from optparse import OptionParser
+
+# Scipy stuff:
+import scipy
+from scipy.fft import ifftn,fftn,fftfreq,fftshift,ifftshift
+from scipy import stats
+import scipy.optimize as opt
